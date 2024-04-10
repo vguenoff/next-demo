ef05be733b71ec4a1d3813fa9ce0e6245726c34a
Vasil Genov <vasil.genov@psinteractive.io>
2024-04-10 16:31:46 +0300

    feat: landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,36 @@
+import Link from 'next/link'
+
 export default function Page() {
   return (
-    <section className="flex min-h-screen flex-col">
-      <h1 className="w-full">Exploring Next.js 14 Main Features</h1>
-      <p className="pt-10">
-        We want to explore new Next.js features because moving to the App Router
-        may be the first time using React features that Next.js builds on top of
-        such as Server Components, Suspense, and more. When combined with new
-        Next.js features such as special files and layouts, migration means new
-        concepts, mental models, and behavioral changes to learn.
-      </p>
+    <section className="flex flex-col">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <h1 className="text-4xl font-bold tracking-tight  sm:text-5xl">
+          Exploring Next.js 14 Main Features
+        </h1>
+        <p className="mt-6 text-lg leading-8 ">
+          We want to explore new Next.js features because moving to the App
+          Router may be the first time using new React features that Next.js
+          builds on top of such as Server Components, Suspense, and more. When
+          combined with new Next.js features such as special files and layouts,
+          migration means new concepts, mental models, and behavioral changes to
+          learn.
+        </p>
+        <div className="mt-10 flex items-center  gap-x-6">
+          <Link
+            href="/01-app-router"
+            className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold  text-white shadow-sm hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+          >
+            Get started
+          </Link>
+          <Link
+            href="https://github.com/vguenoff/next-demo"
+            target="_blank"
+            className="text-sm font-semibold leading-6 text-white"
+          >
+            Demo source code <span aria-hidden="true">→</span>
+          </Link>
+        </div>
+      </div>{' '}
     </section>
   )
 }
--- src/components/BlogSummaryCard.tsx
+++ src/components/BlogSummaryCard.tsx
@@ -1,4 +1,6 @@
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 type BlogSummaryCardProps = {
   slug: string
@@ -11,11 +13,16 @@ export default function BlogSummaryCard({
   title,
   i,
 }: BlogSummaryCardProps) {
+  const pathname = usePathname()
+  const href = `/${slug}`
+
   return (
-    <Link href={`/${slug}`} prefetch={true}>
+    <Link {...{ href }} prefetch={true}>
       <div className="hidden sm:block">
         <p className="m-0">{title}</p>
-        <p className="to-blue-300">→</p>
+        <p>
+          {pathname === href ? <span className="text-red-400">→</span> : '⇢'}
+        </p>
       </div>
       <p className="h-5 w-5 shrink-0 grow-0 rounded-full border to-blue-300 text-center text-xs font-bold leading-5 sm:hidden">
         {i + 1}
