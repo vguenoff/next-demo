aaa1a9c5031a797ab5a17b51a142003ccd50ba30
Vasil Genov <vasil.genov@psinteractive.io>
2024-04-10 18:55:16 +0300

    feat: review 1 and 2

--- src/components/BlogSummaryCard.tsx
+++ src/components/BlogSummaryCard.tsx
@@ -1,4 +1,5 @@
 'use client'
+import clsx from 'clsx'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
@@ -13,19 +14,24 @@ export default function BlogSummaryCard({
   title,
   i,
 }: BlogSummaryCardProps) {
-  const pathname = usePathname()
-  const href = `/${slug}`
+  const selected = usePathname() === `/${slug}`
 
   return (
-    <Link {...{ href }} prefetch={true}>
+    <Link
+      href={`/${slug}`}
+      prefetch={true}
+      className={clsx({ 'font-normal': !selected })}
+    >
       <div className="hidden sm:block">
         <p className="m-0">{title}</p>
-        <p>
-          {pathname === href ? <span className="text-red-400">→</span> : '⇢'}
-        </p>
+        <p>{selected ? <span className="text-red-400">→</span> : '⇢'}</p>
       </div>
       <p className="h-5 w-5 shrink-0 grow-0 rounded-full border to-blue-300 text-center text-xs font-bold leading-5 sm:hidden">
-        {i + 1}
+        {selected ? (
+          <span className="text-red-400">{i + 1}</span>
+        ) : (
+          <span>{i + 1}</span>
+        )}
       </p>
     </Link>
   )
