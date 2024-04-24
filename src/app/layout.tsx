3e89c64b17078a77a6b2dc09527ed5701128d14c
Vasil Genov <vasil.genov@psinteractive.io>
2024-04-24 18:20:43 +0300

    feat: text revision

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ const monoFont = Spline_Sans_Mono({
 
 export const metadata = {
   title: BLOG_TITLE,
-  description: 'A blog about JavaScript',
+  description: 'Demo',
 }
 
 export default async function RootLayout({ children }: PropsWithChildren) {
